b2bd0591520e894251df017a934392872f2c5ff9
Matheus Fernandes <matheusfernandesluz@gmail.com>
2020-01-27 09:19:23 -0400

    Finalizada aplicação

---
+++ web/huntweb/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './styles.css';
+import Header from './components/Header';
+import Main from './pages/main';
+import Routes from './routes';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Routes></Routes>
+    </div>
+  );
+}
+
+export default App;
